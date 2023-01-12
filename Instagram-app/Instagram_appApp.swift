b416abbb24990d09fbf9c2f37b15ad165b567dee
Edgard <20133252@ealoe.ulima.edu.pe>
2023-01-12 09:47:42 -0500

    Initial Commit

---
+++ Instagram-app/Instagram_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_appApp.swift
+//  Instagram-app
+//
+//  Created by EdgardVS on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
